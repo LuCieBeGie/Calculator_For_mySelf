edc9dd002a781577a299196b4ef4707410840653
LuCieBeGie <105434870+LuCieBeGie@users.noreply.github.com>
2023-05-04 12:48:01 +0200

    update

--- script/script.js
+++ script/script.js
@@ -1,7 +1,9 @@
 const changeColorButton = document.querySelector('.color-change-btn')
 console.log(changeColorButton);
 const calculatorTable = document.querySelector('.calculator-table')
-
+const body = document.querySelector('body')
+console.log(body);
 changeColorButton.onclick = function () {
     calculatorTable.classList.toggle('calculator-table-dark')
+    body.classList.toggle('body_dark')
 }

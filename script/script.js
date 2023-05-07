d60159b8d36b35cbc846b545364c767422b2d221
Elenaaa123 <115082303+Elenaaa123@users.noreply.github.com>
2023-05-07 20:00:11 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -11,164 +11,168 @@ const backspaceButton = document.querySelector('[data-backspace]');
 
 let currentValue = '';
 let storedValue = '';
-let currentAction = '';
+let currentAction = null;
 let resultDisplayed = false;
 
 function add(a, b) {
-    return a + b
+  return a + b;
 }
 
 function subtract(a, b) {
-    return a - b
+  return a - b;
 }
 
 function multiply(a, b) {
-    return a * b
+  return a * b;
 }
 
 function divide(a, b) {
-    if (b === 0) {
-        return 'ERROR!'
-    }
-    return a / b
+  if (b === 0) {
+    return 'ERROR!';
+  }
+  return a / b;
 }
 
 function doAction(action, a, b) {
-    switch (action) {
-        case '+':
-            return add(a, b)
-        case '-':
-            return subtract(a, b)
-        case '*':
-            return multiply(a, b)
-        case '/':
-            return divide(a, b)
-        default:
-            return 'Invalid action';
-    }
+  switch (action) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      return 'Invalid action';
+  }
 }
 
 function screenUpdate() {
-    screen.value = currentValue
+  screen.value = currentValue;
 }
 
 function checkMaxLength() {
-    let maxLength = 10
-    if (currentValue.length > maxLength) {
-        currentValue = currentValue.slice(0, maxLength)
-        alert('Maximum length is reached')
-    }
+  let maxLength = 10;
+  if (currentValue.length > maxLength) {
+    currentValue = currentValue.slice(0, maxLength);
+    alert('Maximum length is reached');
+  }
 }
 
 numberButtons.forEach((button) => {
-    button.addEventListener('click', function () {
-        if (resultDisplayed) {
-            currentValue = '';
-            resultDisplayed = false;
-        }
-        if (screen.value === 'ERROR!') {
-            screen.value = '';
-            currentValue = '';
-        }
-        if (button.value === '.') {
-            if (!currentValue.includes('.')) {
-                currentValue += "."
-            }
-        } else if (currentValue == '0') {
-            if (button.value !== ".") {
-                currentValue = button.value
-            }
-        }
-        else {
-            currentValue += button.value
-        }
-        checkMaxLength();
-        screenUpdate();
-    })
+  button.addEventListener('click', function () {
+    if (resultDisplayed) {
+      currentValue = '';
+      resultDisplayed = false;
+    }
+    if (screen.value === 'ERROR!') {
+      screen.value = '';
+      currentValue = '';
+    }
+    if (button.value === '.') {
+      if (!currentValue.includes('.')) {
+        currentValue += '.';
+      }
+    } else if (currentValue == '0') {
+      if (button.value !== '.') {
+        currentValue = button.value;
+      }
+    } else {
+      currentValue += button.value;
+    }
+    checkMaxLength();
+    screenUpdate();
+  });
 });
 
 actionButtons.forEach((el) => {
-    el.addEventListener('click', function () {
-        if (currentValue === '' && storedValue !== '' && currentAction !== '') {
-            currentAction = el.value;
-            console.log(currentAction);
-            return;
-        }
-        if (storedValue !== '') {
-            currentValue = doAction(
-                currentAction,
-                parseFloat(storedValue),
-                parseFloat(currentValue)
-            );
-            screenUpdate();
-        }
-        currentAction = el.value;
-        storedValue = currentValue;
-        currentValue = '';
-    });
-});
-
-clear_all.addEventListener('click', function () {
-    currentValue = ''
-    storedValue = ''
-    currentAction = ''
-    screenUpdate()
-})
-
-clear.addEventListener('click', function () {
-    currentValue = ''
-    screenUpdate()
-})
-
-backspaceButton.addEventListener('click', function () {
-    currentValue = currentValue.slice(0, -1)
-    screenUpdate()
-})
-
-equalButton.addEventListener('click', function () {
-    if (currentValue === '' || storedValue === '' || currentAction === '')
-        return
-    currentValue = doAction(
+  el.addEventListener('click', function () {
+    if (currentValue === '' && storedValue !== '' && currentAction !== null) {
+      currentAction = el.value;
+      console.log(currentAction);
+      return;
+    }
+    if (storedValue !== '') {
+      currentValue = doAction(
         currentAction,
         parseFloat(storedValue),
         parseFloat(currentValue)
-    ).toString()
-    screenUpdate()
-    currentAction = ''
-    storedValue = currentValue
-    resultDisplayed = true;
+      );
+      screenUpdate();
+    }
+    currentAction = el.value;
+    storedValue = currentValue;
+    currentValue = '';
+  });
+});
+
+clear_all.addEventListener('click', function () {
+  currentValue = '';
+  storedValue = '';
+  currentAction = null;
+  screenUpdate();
+});
+
+clear.addEventListener('click', function () {
+  currentValue = '';
+  screenUpdate();
+});
+
+backspaceButton.addEventListener('click', function () {
+  currentValue = currentValue.slice(0, -1);
+  screenUpdate();
+});
+
+equalButton.addEventListener('click', function () {
+  if (currentValue === '' || storedValue === '' || currentAction === '') return;
+  currentValue = doAction(
+    currentAction,
+    parseFloat(storedValue),
+    parseFloat(currentValue)
+  ).toString();
+  screenUpdate();
+  storedValue = '';
+  currentAction = '';
+  resultDisplayed = true;
 });
 
 changeColorButton.onclick = function () {
-    calculatorTable.classList.toggle('calculator-table-dark')
-    body.classList.toggle('body_dark')
-}
+  calculatorTable.classList.toggle('calculator-table-dark');
+  body.classList.toggle('body_dark');
+};
 
-document.addEventListener("keyup", function (e) {
+document.addEventListener(
+  'keyup',
+  function (e) {
     switch (e.which) {
-        case 8:
-            backspaceButton.click()
-            break;
-        case 46:
-            clear_all.click()
-            break
-        case 13:
-            equalButton.click();
-        default:
-            break;
+      case 8:
+        backspaceButton.click();
+        break;
+      case 46:
+        clear_all.click();
+        break;
+      case 13:
+        if (currentAction !== '') {
+          equalButton.click();
+        }
+      default:
+        break;
     }
     if (e.which !== 13) {
-        for (let i = 0; i < numberButtons.length; i++) {
-            let id = numberButtons[i].getAttribute("value");
-            if (id == e.key) {
-                numberButtons[i].click();
-            }
+      for (let i = 0; i < numberButtons.length; i++) {
+        let id = numberButtons[i].getAttribute('value');
+        if (id == e.key) {
+          numberButtons[i].click();
         }
-        for (let i = 0; i < actionButtons.length; i++) {
-            let id = actionButtons[i].getAttribute("value");
-            if (id == e.key) {
-                actionButtons[i].click();
-            }
+      }
+      for (let i = 0; i < actionButtons.length; i++) {
+        let id = actionButtons[i].getAttribute('value');
+        if (id == e.key) {
+          actionButtons[i].click();
         }
+      }
     }
-}, false);
+  },
+  false
+);
